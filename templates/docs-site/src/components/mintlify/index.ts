f24f1e317ac0da19cef59f11ecb149df52e40b72
Arshdeep54 <balarsh535@gmail.com>
2026-08-22 19:20:24 +0530

    Add docs-site template with Mintlify-compatible components

---
+++ templates/docs-site/src/components/mintlify/index.ts
@@ -0,0 +1,6 @@
+export { default as Note } from './Note.astro';
+export { default as Info } from './Info.astro';
+export { default as Tip } from './Tip.astro';
+export { default as Check } from './Check.astro';
+export { default as Warning } from './Warning.astro';
+export { default as Danger } from './Danger.astro';
